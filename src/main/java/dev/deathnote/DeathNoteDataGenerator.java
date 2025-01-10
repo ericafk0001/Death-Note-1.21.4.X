6fbe6de7d43c796dfe52de50589f4470b4cbb0bd
ericafk0001 <dxslyofficial@gmail.com>
2025-01-09 21:37:02 -0500

    Initial commit TWO

---
+++ src/main/java/dev/deathnote/DeathNoteDataGenerator.java
@@ -0,0 +1,11 @@
+package dev.deathnote;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class DeathNoteDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
